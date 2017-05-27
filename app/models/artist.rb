f6780d1cb757dc83782f564988bec89a31bc8c0e
Claudia413 <cmjengelsman@gmail.com>
2017-05-27 12:51:00 +0200

    Add validations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,6 @@
 class Artist < ApplicationRecord
-  has_many :songs
+  has_many :songs, dependent: :destroy
+
+  validates :name, presence: true
+  validates :rating, length: {maximum: 1}
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,7 @@
 class Song < ApplicationRecord
   belongs_to :artist
+
+  validates :title, presence: true
+  validates :rating, length: {maximum: 1}
+  validates :release_date, presence: true
 end
