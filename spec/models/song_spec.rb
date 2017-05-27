7ae1e6dddb9e39bfd7da8a9fd516f161a8f5283d
Claudia413 <cmjengelsman@gmail.com>
2017-05-27 12:24:32 +0200

    Add model tests

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:release_date) }
+    it { is_expected.to validate_numericality_of(:rating).is_at_most(5) }
+  end
+
+    describe "order songs by name in acending order" do
+      let!(:song1) {create :song, title:"Alabama"}
+      let!(:song2) {create :song, title:"Ruby, ruby, ruby!"}
+      let!(:song3) {create :song, title:"Bonus track"}
+
+      it "returns an ordered list of songs from a to z" do
+        expect(Song.order_by_price).to match_array [song1, song2, song3]
+      end
+    end
+
+    describe "order songs by name in decending order" do
+      let!(:song1) {create :song, title:"Alabama"}
+      let!(:song2) {create :song, title:"Ruby, ruby, ruby!"}
+      let!(:song3) {create :song, title:"Bonus track"}
+
+      it "returns an ordered list of songs from z to a" do
+        expect(Song.order_by_price).to match_array [song3, song2, song1]
+      end
+    end
+
 end
