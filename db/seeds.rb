0c43160d50a43d143c0bb39efcd41eb2c143a0f9
Claudia413 <cmjengelsman@gmail.com>
2017-05-27 13:43:10 +0200

    Add songs and artists to seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Artist.destroy_all
+Song.destroy_all
+
+artist1 = Artist.create(name:"Duke Ellington")
+artist2 = Artist.create(name:"Miles Davis")
+artist3 = Artist.create(name:"Nat King Cole")
+artist4 = Artist.create(name:"Oliver Nelson")
+artist3 = Artist.create(name:"Paul Desmond")
+artist4 = Artist.create(name:"John Coltrane")
+
+song1 = Song.create(title:"In a sentimental mood", rating: 4, release_date: 500.days.ago, artist: artist1)
+song2 = Song.create(title:"It never entered my mind", rating: 5, release_date: 900.days.ago, artist: artist2)
+song1 = Song.create(title:"Nature boy", rating: 5, release_date: 500.days.ago, artist: artist3)
+song2 = Song.create(title:"Stolen moments", rating: 3, release_date: 850.days.ago, artist: artist4)
